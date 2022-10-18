51f1a256345d540eb9a104082f886d3e89190c00
ontakunatsu02 <87275762+NtQuerySystemInformation@users.noreply.github.com>
2022-10-17 21:18:40 -0500

    Close to flag, 35%

--- Challenge 9/decrypt_flag.py
+++ Challenge 9/decrypt_flag.py
@@ -4,9 +4,14 @@ import rsa
 
 
 
+def decrypt_rsa():  
+    
 
 
 
+#first 30 bytes are related to keys.
+#Your objective now is deducing what corresponds to what, and take in mind ALSO salsa 20.
+#Take in mind that: Keys are 512 bits
 def main():
     
 
